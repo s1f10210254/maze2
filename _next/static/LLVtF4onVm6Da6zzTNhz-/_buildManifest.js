2c188fe9898d6288f0559a730d90e83beda58af4
s1f10210254 <s1f10210254@users.noreply.github.com>
2023-07-24 13:02:09 +0000

    deploy: 1912be5a23150766da11eeb4a55635328a43dbb8

---
+++ _next/static/LLVtF4onVm6Da6zzTNhz-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5e79be9d39af37d8.css","static/chunks/pages/index-15d3a8b94a09815e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
