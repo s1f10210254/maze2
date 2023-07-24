bcfc502616545820f234561997e36c824cc6279b
s1f10210254 <s1f10210254@users.noreply.github.com>
2023-07-24 03:57:27 +0000

    deploy: c6092b98ac63ee80c8ebf660815872253688f03c

--- _next/static/03drgAil83wKASFPI4zJ3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/893a52beda117a02.css","static/chunks/pages/index-3516a8a2a51cf7e8.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
